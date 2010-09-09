f639c87c6abc1dd58df0ba7b3a361c8e70e11c52
xfun68 <xfun68@gmail.com>
2010-09-09 19:04:40 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lihui::Application.config.secret_token = 'e40afa11adda917cc5b43047fd43712ca443e80d1744cdc11be730dd4302d934005438d5e8c91c53d4b8c880b511dfe8018bd06c2bb897241f40f0b9826502ed'
